030e391aa40fae982abc2d33a64e732de204f833
Kirill Volovitskiy <kiv@sitecore.net>
2017-11-28 10:22:23 +0200

    targets are returned even if a user has no access to the contex item

---
+++ src/Sitecore.Support.28471/ItemWebApi/BadRequestException.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ItemWebApi
+{
+  internal class BadRequestException : Exception
+  {
+    public BadRequestException(string message) : base(message)
+    {
+      Assert.ArgumentNotNull(message, "message");
+    }
+  }
+}
